928f962982db6b33f0eb966c5264a72ebf605c53
PedroFreitasM <130061255+PedroFreitasM@users.noreply.github.com>
2024-10-08 09:04:00 -0300

    Update trab1.py
    Atualização do trabalho

--- trab1.py
+++ trab1.py
@@ -1,22 +1,45 @@
 import random as rd
-soma_dados = rd.randint(1,6) + rd.randint(1,6)
-jogo = []
-print(soma_dados)
-jogo.append(soma_dados)
-if (soma_dados == 7 or soma_dados == 11):
-    print("Voce venceu!")
-elif (soma_dados == 2 or soma_dados == 3 or soma_dados == 12):
-    print("Voce perdeu...")
-else:
+
+#1.a : definição do jogo em si
+def jogo_craps():
     soma_dados = rd.randint(1,6) + rd.randint(1,6)
+    jogo = []
+    print(soma_dados)
     jogo.append(soma_dados)
-    while(soma_dados != jogo[0] and soma_dados != 7):
+    if (soma_dados == 7 or soma_dados == 11):
+        print("Voce venceu!")
+        jogo.append(1)
+    elif (soma_dados == 2 or soma_dados == 3 or soma_dados == 12):
+        print("Voce perdeu...")
+        jogo.append(0)
+    else:
         soma_dados = rd.randint(1,6) + rd.randint(1,6)
         jogo.append(soma_dados)
-    if(soma_dados == jogo[0]):
-        print("Voce venceu!")
-    else:
-        print("Voce perdeu...")
-print("Resultado do jogo:")
-for x in jogo:
-    print(x)    
+        while(soma_dados != jogo[0] and soma_dados != 7):
+            soma_dados = rd.randint(1,6) + rd.randint(1,6)
+            jogo.append(soma_dados)
+        if(soma_dados == jogo[0]):
+            print("Voce venceu!")
+            jogo.append(1)
+        else:
+            print("Voce perdeu...")
+            jogo.append(0)
+    print("Resultado do jogo:")
+    for x in jogo:
+        print(x)   
+    return jogo
+
+#1.b : teste das 5 partidas
+partidas = []
+for x in range(5):
+    res = jogo_craps()
+    partidas.append(res)
+print(partidas)
+
+#1.c : probabilidade de vitória
+soma = 0
+for x in range(1000): #um número alto para garantir...
+    jogo = jogo_craps()
+    soma+=jogo[-1]
+print("Probabilidade de vitoria:")
+print((soma/1000))
