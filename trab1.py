5fc3b54aaaf5d0f8f15c9ef8d3ad4712ec2b920e
PedroFreitasM <130061255+PedroFreitasM@users.noreply.github.com>
2024-10-08 09:46:52 -0300

    Update trab1.py

--- trab1.py
+++ trab1.py
@@ -4,13 +4,13 @@ import random as rd
 def jogo_craps():
     soma_dados = rd.randint(1,6) + rd.randint(1,6)
     jogo = []
-    print(soma_dados)
+    #print(soma_dados)
     jogo.append(soma_dados)
     if (soma_dados == 7 or soma_dados == 11):
-        print("Voce venceu!")
+        #print("Voce venceu!")
         jogo.append(1)
     elif (soma_dados == 2 or soma_dados == 3 or soma_dados == 12):
-        print("Voce perdeu...")
+        #print("Voce perdeu...")
         jogo.append(0)
     else:
         soma_dados = rd.randint(1,6) + rd.randint(1,6)
@@ -19,14 +19,14 @@ def jogo_craps():
             soma_dados = rd.randint(1,6) + rd.randint(1,6)
             jogo.append(soma_dados)
         if(soma_dados == jogo[0]):
-            print("Voce venceu!")
+         #   print("Voce venceu!")
             jogo.append(1)
         else:
-            print("Voce perdeu...")
+          #  print("Voce perdeu...")
             jogo.append(0)
-    print("Resultado do jogo:")
-    for x in jogo:
-        print(x)   
+    #print("Resultado do jogo:")
+    #for x in jogo:
+    #    print(x)   
     return jogo
 
 #1.b : teste das 5 partidas
@@ -43,3 +43,12 @@ for x in range(1000): #um número alto para garantir...
     soma+=jogo[-1]
 print("Probabilidade de vitoria:")
 print((soma/1000))
+
+#1.d : Como a probabilidade de ganhar ou perder é igual, o lucro esperado é 0, tanto para somente uma jogada, quanto para 1000.
+'''
+1.e : O cálculo para a variância é E(X²) - E(X)².
+Como a probabilidade de ganhar e perder é igual, temos que:
+E(X) = 1 * 0.5 + -1 * 0.5 = 0 = E(X)²
+E(X²) = (1)² * 0.5 + (-1)² * 0.5 = 1
+Logo, E(X²) - E(X)² = 1, que é a variância.
+'''
